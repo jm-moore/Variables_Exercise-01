da53672fc83212de305dee78db0aef0baf3040a6
jm-moore <jmmoore9358@gmail.com>
2022-08-05 13:51:27 -0500

    completed Todos

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -10,7 +10,13 @@ namespace Variables_Exercise_01
             // - Once finished save, commit, and then push to GitHub!
 
             // TODO: Declare 3 constansts
+            const string firstName = "Joshua";
+            const int yearOfBirth = 1998;
+            const string lastName = "Moore";
 
+            System.Console.WriteLine($"{firstName}");
+            System.Console.WriteLine($"{lastName}");
+            System.Console.WriteLine(yearOfBirth);
             #region Types
             // string
             // char
@@ -19,6 +25,27 @@ namespace Variables_Exercise_01
             // double
             // decimal
             #endregion
+
+            string a;
+            char b;
+            bool c;
+            int d;
+            double e;
+            decimal f;
+
+            a = "Hello";
+            b = 'J';
+            c = false;
+            d = 7;
+            e = 3.14;
+            f = 3.14m;
+
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+            System.Console.WriteLine(c);
+            System.Console.WriteLine(d);
+            System.Console.WriteLine(e);
+            System.Console.WriteLine(f);
 
             // TODO: Declare a variable for each of the types above
             // Make sure to use C# naming convention best practices 
